e2560b738434c6bb6594c6de9cd900252487597d
Emilio Gomez <emiliogmztj@gmail.com>
2026-03-11 15:03:43 -0500

    Updated to Next.Js to Github page template.

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: 'export',
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
